43c60c11bf61d7627629be5fa40b22ef54a2158d
wumianhuan <wumianhuan@coocaa.com>
2019-03-12 10:58:59 +0800

    在hosts里添加 127.0.0.1 www.rice.com 不要用localhost测试!!! registry.addMapping("/**");

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -2,9 +2,24 @@ package com.company.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi.By Get....";
+    }
+
+    @PostMapping("/helloPost")
+    public String helloPost(){
+        return "helloPost.By Post.";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
